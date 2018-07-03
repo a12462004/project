869b66b139187d1191ff891920f03f4be192bb7b
ET <a12462004@gmail.com>
2018-07-04 04:22:19 +0800

    modify weather_controller to show a right week data

---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,24 @@
+class WeatherController < ApplicationController
+  def index
+  	h = Time.now.strftime("%H").to_i #當前時間是幾點，轉換成INT
+  	t = Time.now.change(hour: 6).iso8601 #當前時間
+  	a=[]
+  	#用for迴圈找出一週的日期，，判斷當前時間點是位於早上還是晚上，寫入a陣列裡
+  	for i in 0..6
+		if h < 18
+  			t = Time.now + i.days
+  			t = t.change(hour: 6).iso8601
+  			a[i]=t
+		else
+			t = Time.now + i.days
+  			t = t.change(hour: 6).iso8601
+  			a[i] = t
+		end	  		
+  	end
+  	# 只有當天
+  	# t = Time.now.strftime("%Y-%m-%d")
+  	# Weather.where("location_name = ? and date(time) = ?",location,t)
+  	location = "臺北市"
+  	@weathers = Weather.where("location_name = ? and time IN (?) ",location,a)
+  end
+end
